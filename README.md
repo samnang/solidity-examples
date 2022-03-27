648940593a42a1fc06f05a035cd8fabc83fe2833
Samnang Chhun <samnang.chhun@gmail.com>
2022-03-27 17:35:54 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Crypto Market Pool - Solidity
+
+This is a collection of example codes from  [Crypto Market Pool - Solidity](https://cryptomarketpool.com/getting-started-with-solidity/). Feel free to clone it and playaround because I have added tests for each contract's example on the website.
+
+## How to Run Examples
+
+```sh
+$ npx hardhat test
+```
---
+++ test/02_constructors/01_my_contract.js
@@ -0,0 +1,21 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Data Types / MyContract", function () {
+  let contract;
+  let owner;
+
+  beforeEach(async () => {
+    [owner] = await ethers.getSigners();
+    const Contract = await ethers.getContractFactory("contracts/02_constructors/01_MyContract.sol:MyContract", owner);
+    contract = await Contract.deploy(5, 10);
+    await contract.deployed();
+  });
+
+  it("Should be able to verify all variables", async function () {
+    expect(await contract.x()).to.equal(5);
+    expect(await contract.y()).to.equal(10);
+    expect(await contract.owner()).to.equal(owner.address);
+    expect(await contract.createdAt()).not.to.be.null;
+  });
+});
