9d883f9afd5fa0943a2374011f416fe2abcf3661
Samnang Chhun <samnang.chhun@gmail.com>
2022-03-27 22:06:47 +0700

    Add Visibility examples

---
+++ test/05_visibility/01_my_contract.js
@@ -0,0 +1,20 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Functions / MyContract", function () {
+  let contract;
+
+  beforeEach(async () => {
+    const Contract = await ethers.getContractFactory("contracts/05_visibility/MyContract.sol:MyContract");
+    contract = await Contract.deploy();
+    await contract.deployed();
+  });
+
+  it("Should be able to verify all variables", async function () {
+    await contract.setName("John Doe")
+    expect(await contract.getName()).to.equal("John Doe");
+    expect(await contract.externalFunction()).to.equal("external-function");
+    expect(await contract.id()).to.equal("123");
+    expect(await contract.getAge()).to.equal(35);
+  });
+});
