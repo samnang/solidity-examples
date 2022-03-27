d8817c5462815a6518807ce98e18b8f4380842df
Samnang Chhun <samnang.chhun@gmail.com>
2022-03-27 22:47:45 +0700

    Add Function Modifiers examples

---
+++ contracts/07_function_modifiers/MyContract.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract MyContract {
+  address public owner;
+  string private name;
+
+  constructor () {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Not owner");
+    _;
+  }
+
+  function setName(string memory newName) public onlyOwner{
+    name = newName;
+  }
+
+  function getName() public view returns(string memory) {
+    return name;
+  }
+}
---
+++ test/07_function_modifiers/01_my_contract.js
@@ -0,0 +1,20 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Functions / MyContract", function () {
+  let contract;
+
+  beforeEach(async () => {
+    const Contract = await ethers.getContractFactory("contracts/07_function_modifiers/MyContract.sol:MyContract");
+    contract = await Contract.deploy();
+    await contract.deployed();
+  });
+
+  it("Should be able to verify all variables", async function () {
+    await contract.setName("John Doe");
+    expect(await contract.getName()).to.equal("John Doe");
+
+    const [owner, other] = await ethers.getSigners();
+    await expect(contract.connect(other).setName("Failed")).to.be.revertedWith("Not owner");
+  });
+});
