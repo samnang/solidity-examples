4ddd9b0ac59afa8ec88e9f7c3617ac805ebe8fcb
Samnang Chhun <samnang.chhun@gmail.com>
2022-04-03 14:36:15 +0700

    Add Payable Modifier examples

---
+++ contracts/17_payable_modifier/MyContract.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract MyContract {
+  address payable public owner;
+
+  constructor() payable {
+    owner = payable(msg.sender);
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Not owner");
+    _;
+  }
+
+  function withdraw(uint amount) public onlyOwner {
+    owner.transfer(amount);
+  }
+
+  function transfer(address payable _to, uint amount) public onlyOwner {
+    _to.transfer(amount);
+  }
+}
---
+++ test/17_payable_modifier/payable_modifier_test.js
@@ -0,0 +1,26 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Payable Modifier", function () {
+  let contract;
+  let owner, signer;
+
+  beforeEach(async () => {
+    [owner, signer] = await ethers.getSigners();
+    const Contract = await ethers.getContractFactory("contracts/17_payable_modifier/MyContract.sol:MyContract", owner);
+    contract = await Contract.deploy({value: 100});
+    await contract.deployed();
+  });
+
+  it("Should be able to verify all variables", async function () {
+    expect(await contract.provider.getBalance(contract.address)).to.equal(100)
+
+    await contract.withdraw(5);
+    expect(await contract.provider.getBalance(contract.address)).to.equal(95)
+
+    currentBalance = await contract.provider.getBalance(signer.address)
+    await contract.transfer(signer.address, 10);
+    expect(await contract.provider.getBalance(contract.address)).to.equal(85)
+    expect(await contract.provider.getBalance(signer.address)).to.equal(BigInt(currentBalance) + BigInt(10))
+  });
+});
