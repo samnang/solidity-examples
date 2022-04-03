be2ec297c1f3a68071636b2ea2d76acedd4a6442
Samnang Chhun <samnang.chhun@gmail.com>
2022-04-03 12:29:06 +0700

    Add Array examples

---
+++ contracts/13_array/MyContract.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract MyContract {
+  uint[] public myArray; //this is a dynamic array of type uint
+  uint[] public myArray2 = [1, 2, 3, 4]; //this is a dynamic array with 1, 2 and 3 as default values
+  uint[10] public myFixedSizeArray; //this is a fixed size array of type uint
+
+  uint[] intergerArray;       //sample array of integers
+  bool[] boolArray;           //sample array of booleans
+  address[] addressArray;     //sample array of address
+
+  function pushIsToAdd(uint i) public {
+    myArray.push(i);
+  }
+
+  function getItemInArray(uint index) public view returns(uint) {
+    return myArray[index];
+  }
+
+  function updateTheArray(uint locationInArray, uint valueToChange) public {
+    myArray[locationInArray] = valueToChange;
+  }
+
+  function remove(uint index) public {
+    delete myArray[index];
+  }
+
+  function removeAndCompact(uint index) public {
+    myArray[index] = myArray[myArray.length - 1];
+    myArray.pop();
+  }
+
+  function getLegth() public view returns(uint) {
+    return myArray.length;
+  }
+}
---
+++ test/13_array/array_test.js
@@ -0,0 +1,33 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Array", function () {
+  let contract;
+
+  beforeEach(async () => {
+    const Contract = await ethers.getContractFactory("contracts/13_array/MyContract.sol:MyContract");
+    contract = await Contract.deploy();
+    await contract.deployed();
+  });
+
+  it("Should be able to verify all variables", async function () {
+    await contract.pushIsToAdd(1);
+    await contract.pushIsToAdd(2);
+    await contract.pushIsToAdd(3);
+    await contract.pushIsToAdd(4); // Array [1, 2, 3, 4]
+
+    expect(await contract.getItemInArray(1)).to.equal(2);
+
+    await contract.updateTheArray(1, 5); // Array [1, 5, 3, 4]
+    expect(await contract.getItemInArray(1)).to.equal(5);
+
+    await contract.remove(1); // Array [1, 0, 3, 4]
+    expect(await contract.getItemInArray(1)).to.equal(0);
+    expect(await contract.getLegth()).to.equal(4);
+
+    await contract.removeAndCompact(1); // Array [1, 4, 3]
+    expect(await contract.getItemInArray(1)).to.equal(4);
+    expect(await contract.getItemInArray(2)).to.equal(3);
+    expect(await contract.getLegth()).to.equal(3);
+  });
+});
