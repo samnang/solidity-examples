1fec32af98b2bfebd6a64e76661d8c3049b2eb89
Samnang Chhun <samnang.chhun@gmail.com>
2022-08-14 16:27:20 +0700

    Remove hardhat console

--- contracts/sample_apps/10_vault/Vault.sol
+++ contracts/sample_apps/10_vault/Vault.sol
@@ -8,8 +8,6 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-import "hardhat/console.sol";
-
 contract Vault is ERC20 {
   IERC20 public immutable token;
 
