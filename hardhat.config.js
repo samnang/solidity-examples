1d000aa39756e69421bf7f422f56e0f43b88a29d
Samnang Chhun <samnang.chhun@gmail.com>
2022-09-19 11:38:50 +0700

    Fix tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,9 @@ require("dotenv/config");
 require("@nomiclabs/hardhat-etherscan");
 const { ethers } = require("ethers");
 
+const DEPLOYER_PRIVATE_KEY =
+  process.env.DEPLOYER_PRIVATE_KEY || ethers.Wallet.createRandom().privateKey;
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -24,7 +27,7 @@ module.exports = {
   networks: {
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+      accounts: [DEPLOYER_PRIVATE_KEY],
     },
   },
   etherscan: {
