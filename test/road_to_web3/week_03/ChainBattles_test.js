65389cebb736db2325ba84a7ee063e94ea27e53f
Samnang Chhun <samnang.chhun@gmail.com>
2022-09-22 00:45:17 +0700

    Add tests for Road to Web3 - Week 03

---
+++ test/road_to_web3/week_03/ChainBattles_test.js
@@ -0,0 +1,33 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Road to Web3 / Week 02 / Buy Me A Coffee", function () {
+  let contract;
+  let deployer, minter;
+
+  beforeEach(async () => {
+    [deployer, minter] = await ethers.getSigners();
+    const Contract = await ethers.getContractFactory(
+      "contracts/road_to_web3/week_03/ChainBattles.sol:ChainBattles"
+    );
+    contract = await Contract.deploy();
+    await contract.deployed();
+  });
+
+  it("Should mint a new NFT token", async function () {
+    await contract.connect(minter).mint();
+
+    newMintedTokenId = 1;
+    expect(await contract.ownerOf(newMintedTokenId)).to.eq(minter.address);
+    expect(await contract.getLevels(newMintedTokenId)).to.eq("0");
+  });
+
+  it("Should be able to train", async function () {
+    await contract.connect(minter).mint();
+
+    newMintedTokenId = 1;
+    await contract.connect(minter).train(newMintedTokenId);
+
+    expect(await contract.getLevels(newMintedTokenId)).to.eq("1");
+  });
+});
